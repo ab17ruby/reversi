cff52e940f13626eab623ee16aa41042c47282bd
Arif DOĞRU <adogru@gtu.edu.tr>
2017-02-07 16:13:05 +0200

    Mission complated
    Revrsi 1.0 version

--- Run.rb
+++ Run.rb
@@ -1,8 +1,34 @@
 require './lib/game'
 
+puts `clear`
 run = Reversi::Game.new
 run.initialize_board
 run.display_board
-run.check_move(4,2,'X','O')
-puts "--------------"
-run.display_board
+
+
+while !run.finish?
+	puts "1. oyuncu (X) koordinat gir örn: 1,5 => (satır,sütun) "
+	puts "--------------"
+	puts "Çıkmak için q girin"
+	a = gets.chomp()
+	break if a.downcase() == 'q'
+
+	koordinat_array = a.split(',')
+	kor_x = koordinat_array[1].to_i - 1
+	kor_y = koordinat_array[0].to_i - 1 
+	run.check_move(kor_x,kor_y,'X','O')
+	run.display_board
+
+	puts "2. oyuncu (O) koordinat gir örn: 2,5 => (satır,sütun) "
+	puts "--------------"
+	b = gets.chomp()
+	
+	break if b.downcase() == 'q'
+
+	koordinat = b.split(',')
+	kor2_x = koordinat[1].to_i - 1
+	kor2_y = koordinat[0].to_i - 1 
+	run.check_move(kor2_x,kor2_y,'O','X')
+	run.display_board
+end
+run.score
--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,11 @@
+require "colored"
+
 module Reversi
 	class Game
 	  attr_accessor  :board
 	  def initialize_board
-	    @board = Array.new(8) { Array.new(8) }
-	    (0..7).each do |row|
-	      (0..7).each do |cell|
-	        @board[row][cell]='*'
-	      end
-	    end
+	    @board = Array.new(8) { Array.new(8) { '*' } }
+	    
 	    @board[3][3] = 'X'
 	    @board[3][4] = 'O'
 	    @board[4][3] = 'O'
@@ -15,133 +13,337 @@ module Reversi
 	  end
 
 	  def check_move(row,column,option,food)
-	    check_right(row,column,option,food)
-	    check_north_east(row,column,option,food)
-	    check_north_west(row,column,option,food)
+		coor_right = check_right(row,column,option,food)
+		move_right(coor_right, row ,column, option) if coor_right
+	   
+
+		coor_ne = check_north_east(row,column,option,food)
+		move_ne(coor_ne,row,column,option) if coor_ne
+
+
+	    coor_nw = check_north_west(row,column,option,food)
+	    move_nw(coor_nw,row,column,option) if coor_nw
+
+	    coor_left = check_left(row,column,option,food)
+	    move_left(coor_left, row ,column, option) if coor_left
+
+	    coor_up = check_up(row,column,option,food)
+	    move_up(coor_up, row ,column, option) if coor_up
+
+	    coor_down = check_down(row,column,option,food)
+	    move_down(coor_down, row ,column, option) if coor_down
+
+	    coor_sw = check_south_west(row,column,option,food)
+	    move_sw(coor_sw, row ,column, option) if coor_sw	    
+
+	    coor_se = check_south_east(row,column,option,food)
+	    move_se(coor_se, row ,column, option) if coor_se
 	  end
 
-	  def check_right(row,column,option,food)
-	  	flag = false
-	    if @board[row][column] == '*'
+	  def move_up(coor,row,column,option)
+	  	
+	  	i = coor[:x]
+	  	j = coor[:y]
+	  	while i <= row
+	  		@board[i][j] = option
+	  		i +=1
+	  	end
 
-	      i=column
-	      profit = 0
-	      
-	      while i < 7 do
-
-	        if @board[row][i+1] == food
-	          puts "sag da food var"
-	          profit +=1
-	          i +=1
-	        end
-	        break if i == 7
-	        if @board[row][i] == option and profit!=0
-	          flag = true
-	          break
-	        end
-	        i +=1
-	      end
-	      if flag
-	        while i >= column do
-	          @board[row][i] = option
-	          i-=1
-	        end
-	      end
-	    end
 	  end
 
-	  def check_north_east(row,column,option,food)
-	  	flag = false
-	    if @board[row][column] == '*'
+	  def move_down(coor,row,column,option)
+	  	
+	  	i = coor[:x]
+	  	j = coor[:y]
+	  	while i >= row
+	  		@board[i][j] = option
+	  		i -=1
+	  	end
 
-	      i=row
-	      j=column
-	      profit = 0
-	      
-	      while i > 0 and j < 7 do
-
-	        if @board[i-1][j+1] == food
-	          puts "üst sag da food var"
-	          profit +=1
-	          i -=1
-	          j +=1
-	        end
-	        break if i == 0 or j==7
-	        if @board[i][j] == option and profit!=0
-	          flag = true
-	          break
-	        end
-	        i -= 1
-	        j += 1
-	      end
-	      if flag
-	        while i <= row and j>= column do
-	          @board[i][j] = option
-	          i += 1
-	          j -= 1
-	        end
-	      end
-	    end
 	  end
 
-		def check_north_west(row,column,option,food)
-			flag = false
-		    if @board[row][column] == '*'
+	  def move_ne(coor,row,column,option)
+	  	
 
-		      i=row
-		      j=column
-		      profit = 0
-		      
-		      while i > 0 and j > 0 do
+	  	i = coor[:x]
+	  	j = coor[:y]
+	  	while i <= row and j >= column
+	  		@board[i][j] = option
+	  		i +=1
+	  		j -=1
+	  	end
+	  end
 
-		        if @board[i-1][j-1] == food
-		          puts "üst sag da food var"
-		          profit +=1
-		          i -=1
-		          j -=1
-		        end
-		        break if i == 0 or j==0
-		        if @board[i][j] == option and profit!=0
-		          flag = true
-		          break
-		        end
-		        i -= 1
-		        j -= 1
-		      end
-		      if flag
-		        while i <= row and j<= column do
-		          @board[i][j] = option
-		          i += 1
-		          j += 1
-		        end
-		      end
-		    end
+	  def move_nw(coor, row,column,option)
+	  	
+
+	  	i = coor[:x]
+	  	j = coor[:y]
+	  	while i <= row and j <= column
+	  		@board[i][j] = option
+	  		i +=1
+	  		j +=1
+	  	end
+	  end
+	  def move_sw(coor, row,column,option)
+	  	
+
+	  	i = coor[:x]
+	  	j = coor[:y]
+	  	while i >= row and j <= column
+	  		@board[i][j] = option
+	  		i -=1
+	  		j +=1
+	  	end
+	  end
+	  def move_se(coor, row,column,option)
+	  	
+
+	  	i = coor[:x]
+	  	j = coor[:y]
+	  	while i >= row and j >= column
+	  		@board[i][j] = option
+	  		i -=1
+	  		j -=1
+	  	end
+	  end
+
+	  def move_right(coor,row,column,option)
+	  	j = coor[:y]
+	  	while j >= column
+	  		@board[row][j] = option
+	  		j-=1
+	  	end
+	  end
+
+	  def move_left(coor,row,column,option)
+	  	j = coor[:y]
+	  	while j <= column
+	  		@board[row][j] = option
+	  		j+=1
+	  	end 	
+
+
+	  end
+		def check_right(row,column,option,food)
+			i, j, profit = row, column, 0
+			if @board[row][column] == '*'
+				while j < 7 
+					j += 1
+
+					if @board[i][j] == food
+						
+
+						profit +=1
+					end
+
+					break if j == 7
+
+					return {x: i ,y: j} if @board[i][j] == option and profit != 0
+				end
+
+			end
+
+			false
+		end
+		
+		def check_left(row,column,option,food)
+			i, j, profit = row, column, 0
+			if @board[row][column] == '*'
+				while j > 0 
+					j -= 1
+
+					if @board[i][j] == food
+						
+
+						profit +=1
+					end
+
+					break if j == 0
+
+					return {x: i ,y: j} if @board[i][j] == option and profit != 0
+				end
+
+			end
+
+			false
+
 		end
 
-	  def display_board
-	      @board.each do |i|
-	        puts i.each {|p| p}.join(" ")
-	      end
-	  end
+		def check_up(row,column,option,food)
+			i, j, profit = row, column, 0
+			if @board[row][column] == '*'
+				while i > 0 
+					i -= 1
+					
+
+					if @board[i][j] == food
+						
+
+						profit +=1
+					end
+
+					break if i.zero?
+
+					return {x: i ,y: j} if @board[i][j] == option and profit != 0
+				end
+
+			end
+
+			false
+
+		end
+		def check_down(row,column,option,food)
+			i, j, profit = row, column, 0
+			if @board[row][column] == '*'
+				while i < 7 
+					i += 1
+					
+
+					if @board[i][j] == food
+						
+
+						profit +=1
+					end
+
+					break if i == 7
+
+					return {x: i ,y: j} if @board[i][j] == option and profit != 0
+				end
+
+			end
+
+			false
+
+		end		
+
+		def check_north_east(row,column,option,food)
+			i, j, profit = row, column, 0
+			if @board[row][column] == '*'
+				while i > 0 and j < 7
+					i -= 1
+					j += 1
+
+					if @board[i][j] == food
+						
+
+						profit +=1
+					end
+
+					break if i.zero? or j == 7
+
+					return {x: i ,y: j} if @board[i][j] == option and profit != 0
+				end
+
+			end
+
+			false
+		end
+
+	def check_north_west(row,column,option,food)
+		i, j, profit = row, column, 0
+	    if @board[row][column] == '*'
+			while i > 0 and j > 0 
+				i -= 1
+				j -= 1
+
+				if @board[i][j] == food
+					
+
+					profit +=1
+				end
+
+				break if i.zero? or j.zero?
+
+				return {x: i ,y: j} if @board[i][j] == option and profit != 0
+			end
+
+		end
+
+	    false
+	end
+	def check_south_west(row,column,option,food)
+		i, j, profit = row, column, 0
+	    if @board[row][column] == '*'
+			while i < 7 and j > 0 
+				i += 1
+				j -= 1
+
+				if @board[i][j] == food
+					
+
+					profit +=1
+				end
+
+				break if i == 7 or j.zero?
+
+				return {x: i ,y: j} if @board[i][j] == option and profit != 0
+			end
+
+		end
+
+	    false
+	end
+		def check_south_east(row,column,option,food)
+			i, j, profit = row, column, 0
+			if @board[row][column] == '*'
+				while i < 7 and j < 7
+					i += 1
+					j += 1
+
+					if @board[i][j] == food
+						
+
+						profit +=1
+					end
+
+					break if i == 7 or j == 7
+
+					return {x: i ,y: j} if @board[i][j] == option and profit != 0
+				end
+
+			end
+
+			false
+		end	
+
+
+	def finish?
+		!@board.any? { |row| row.any? { |column| column == '*' } }
+	end
+
+	def score
+		temp = @board.join.to_s
+		puts "****************************"
+		puts "*  1. oyuncunun skoru : #{temp.count('X')}  *"
+		puts "*  2. oyuncunun skoru : #{temp.count('O')}  *"
+		puts "****************************"
+		first = temp.count('X')
+		second = temp.count('O')
+
+		if first > second
+			puts "1. Oyuncu Kazandı!  YÜRÜ BE.."
+		elsif second > first
+			puts "2. Oyuncu Kazandı!  YÜRÜ BE.."
+		else
+			puts "Berabere Tamamlandı! TEKRAR BEKLERİZ"
+		end
+
+	    
+	end
+
+		def display_board
+			@board.each_with_index do |row, i|
+				print "#{i.next} "
+				puts row.map { |column| 
+					column = column.red if column == 'X' 
+					column = column.green if column == 'O' 
+					column
+				}.join(' ')
+			end
+			print "0 "
+			8.times { |i| print "#{i.next} " }
+			puts ''
+		end
 	end
 end
-
-###### TEST CODE  ########
-
-=begin
-
-sağ üst çapraz kontrol
-
-puts "-----------------"
-obj.check_move(5,2,'X','O')
-obj.display_board
-puts "-----------------"
-obj.check_move(6,1,'X','O')
-obj.display_board
- sag kontrol
-obj.check_move(3,2,'X','O')
-obj.display_board
-puts "-----------------"
-obj.check_move(3,1,'X','O')
-obj.display_board
-=end
